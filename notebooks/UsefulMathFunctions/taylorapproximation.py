fa95642a3516e88ef8597f09217d85707093c342
Kedy Edme <32274439+kekeedme@users.noreply.github.com>
2023-01-21 11:56:26 +0100

    -Made the docstring better (even though it fails pylint, and I don't understand why). -Change the function name to respect snake case naming conventions (which yielded higher score with pylint) -Added a little document for error for derivatives

--- notebooks/UsefulMathFunctions/taylorapproximation.py
+++ notebooks/UsefulMathFunctions/taylorapproximation.py
@@ -1,17 +1,18 @@
 from math import factorial as fact
 
 
-def nDerivative(function, order, argument, stepsize):
+def nderivative(function, order, argument, stepsize):
     """This function outputs the nth derivative
-    of a user-specified function
-    It takes as input the following:
-    function: a user-defined function
-    order: the order of the derivative of interests
-    argument: the value at which the derivative is to be evaluated
-    stepsize: the size of the intervals, the smaller, the better
-    Author : Kedy Edme
-    Date : Jan 20th 2023"""
+    of a user-specified function.
 
+    It takes as input the following
+    :param function: a user-defined function
+    :param order: the order of the derivative of interests
+    :param argument: the value at which the derivative is to be evaluated
+    :param stepsize: the size of the intervals, the smaller, the better
+    Author : Kedy Edme
+    Date : Jan 20th 2023
+    """
     # declaring the derivative variable and setting it to zero
     derivative = 0
 
@@ -29,21 +30,23 @@ def nDerivative(function, order, argument, stepsize):
 
 def taylorapprox(function, argument, a_value, maxorder, stepsize):
     """This function outputs the taylor approximation
-    to nth-order of a user-specified function. It takes:
-    function: the user-specified function
-    argument: the argument at which we evaluate the approximation
-              usually x-values for 1-D functions, can be a list.
-    a_value: the center around which the user seeks the approximation
-    maxorder: the maximum order of the approximation
-    stepsize: the stepsize used to evaluate the derivatives
-    Author: Kedy Edme
-    Date: Jan 2oth 2023"""
+    to nth-order of a user-specified function.
 
+    It takes:
+    :param function: the user-specified function
+    :param argument: the argument at which we evaluate the approximation
+              usually x-values for 1-D functions, can be a list.
+    :param a_value: the center around which the user seeks the approximation
+    :param maxorder: the maximum order of the approximation
+    :param stepsize: the stepsize used to evaluate the derivatives
+    Author: Kedy Edme
+    Date: Jan 20th 2023
+    """
     approximation = 0
 
     for order in range(maxorder + 1):
         approximation = approximation + (
-            nDerivative(function, order, a_value, stepsize)
+            nderivative(function, order, a_value, stepsize)
             * (argument - a_value) ** order
         ) / fact(order)
     return approximation
